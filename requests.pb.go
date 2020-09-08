417c3f02f701b9b948a0139928c8b73fa6501ea2
Aleksandr Stevens <aleks.stevens@protonmail.com>
2020-09-08 13:07:58 -0700

    wrote return code protobuf

--- requests.pb.go
+++ requests.pb.go
@@ -426,6 +426,171 @@ func (x *InitialRequest) GetSerial() []byte {
 	return nil
 }
 
+type VerificationResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Signatures []*VerificationResponse_SignaturePair `protobuf:"bytes,1,rep,name=signatures,proto3" json:"signatures,omitempty"`
+}
+
+func (x *VerificationResponse) Reset() {
+	*x = VerificationResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_requests_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VerificationResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VerificationResponse) ProtoMessage() {}
+
+func (x *VerificationResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_requests_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VerificationResponse.ProtoReflect.Descriptor instead.
+func (*VerificationResponse) Descriptor() ([]byte, []int) {
+	return file_requests_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *VerificationResponse) GetSignatures() []*VerificationResponse_SignaturePair {
+	if x != nil {
+		return x.Signatures
+	}
+	return nil
+}
+
+type SubnetResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Workers    []string                      `protobuf:"bytes,1,rep,name=workers,proto3" json:"workers,omitempty"`
+	Bystanders []string                      `protobuf:"bytes,2,rep,name=bystanders,proto3" json:"bystanders,omitempty"`
+	Addresses  []*SubnetResponse_AddressPair `protobuf:"bytes,3,rep,name=addresses,proto3" json:"addresses,omitempty"`
+	Reward     float64                       `protobuf:"fixed64,4,opt,name=reward,proto3" json:"reward,omitempty"`
+}
+
+func (x *SubnetResponse) Reset() {
+	*x = SubnetResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_requests_proto_msgTypes[8]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *SubnetResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*SubnetResponse) ProtoMessage() {}
+
+func (x *SubnetResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_requests_proto_msgTypes[8]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use SubnetResponse.ProtoReflect.Descriptor instead.
+func (*SubnetResponse) Descriptor() ([]byte, []int) {
+	return file_requests_proto_rawDescGZIP(), []int{8}
+}
+
+func (x *SubnetResponse) GetWorkers() []string {
+	if x != nil {
+		return x.Workers
+	}
+	return nil
+}
+
+func (x *SubnetResponse) GetBystanders() []string {
+	if x != nil {
+		return x.Bystanders
+	}
+	return nil
+}
+
+func (x *SubnetResponse) GetAddresses() []*SubnetResponse_AddressPair {
+	if x != nil {
+		return x.Addresses
+	}
+	return nil
+}
+
+func (x *SubnetResponse) GetReward() float64 {
+	if x != nil {
+		return x.Reward
+	}
+	return 0
+}
+
+type ReturnCodeResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	ReturnCode int32 `protobuf:"varint,1,opt,name=returnCode,proto3" json:"returnCode,omitempty"`
+}
+
+func (x *ReturnCodeResponse) Reset() {
+	*x = ReturnCodeResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_requests_proto_msgTypes[9]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ReturnCodeResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ReturnCodeResponse) ProtoMessage() {}
+
+func (x *ReturnCodeResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_requests_proto_msgTypes[9]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ReturnCodeResponse.ProtoReflect.Descriptor instead.
+func (*ReturnCodeResponse) Descriptor() ([]byte, []int) {
+	return file_requests_proto_rawDescGZIP(), []int{9}
+}
+
+func (x *ReturnCodeResponse) GetReturnCode() int32 {
+	if x != nil {
+		return x.ReturnCode
+	}
+	return 0
+}
+
 type VerificationRequest_Keypair struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -438,7 +603,7 @@ type VerificationRequest_Keypair struct {
 func (x *VerificationRequest_Keypair) Reset() {
 	*x = VerificationRequest_Keypair{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_requests_proto_msgTypes[7]
+		mi := &file_requests_proto_msgTypes[10]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -451,7 +616,7 @@ func (x *VerificationRequest_Keypair) String() string {
 func (*VerificationRequest_Keypair) ProtoMessage() {}
 
 func (x *VerificationRequest_Keypair) ProtoReflect() protoreflect.Message {
-	mi := &file_requests_proto_msgTypes[7]
+	mi := &file_requests_proto_msgTypes[10]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -477,6 +642,116 @@ func (x *VerificationRequest_Keypair) GetId() string {
 func (x *VerificationRequest_Keypair) GetKey() []byte {
 	if x != nil {
 		return x.Key
+	}
+	return nil
+}
+
+type VerificationResponse_SignaturePair struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id        string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Signature []byte `protobuf:"bytes,2,opt,name=signature,proto3" json:"signature,omitempty"`
+}
+
+func (x *VerificationResponse_SignaturePair) Reset() {
+	*x = VerificationResponse_SignaturePair{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_requests_proto_msgTypes[11]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VerificationResponse_SignaturePair) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VerificationResponse_SignaturePair) ProtoMessage() {}
+
+func (x *VerificationResponse_SignaturePair) ProtoReflect() protoreflect.Message {
+	mi := &file_requests_proto_msgTypes[11]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VerificationResponse_SignaturePair.ProtoReflect.Descriptor instead.
+func (*VerificationResponse_SignaturePair) Descriptor() ([]byte, []int) {
+	return file_requests_proto_rawDescGZIP(), []int{7, 0}
+}
+
+func (x *VerificationResponse_SignaturePair) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *VerificationResponse_SignaturePair) GetSignature() []byte {
+	if x != nil {
+		return x.Signature
+	}
+	return nil
+}
+
+type SubnetResponse_AddressPair struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id      string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Address []string `protobuf:"bytes,2,rep,name=address,proto3" json:"address,omitempty"`
+}
+
+func (x *SubnetResponse_AddressPair) Reset() {
+	*x = SubnetResponse_AddressPair{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_requests_proto_msgTypes[12]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *SubnetResponse_AddressPair) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*SubnetResponse_AddressPair) ProtoMessage() {}
+
+func (x *SubnetResponse_AddressPair) ProtoReflect() protoreflect.Message {
+	mi := &file_requests_proto_msgTypes[12]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use SubnetResponse_AddressPair.ProtoReflect.Descriptor instead.
+func (*SubnetResponse_AddressPair) Descriptor() ([]byte, []int) {
+	return file_requests_proto_rawDescGZIP(), []int{8, 0}
+}
+
+func (x *SubnetResponse_AddressPair) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *SubnetResponse_AddressPair) GetAddress() []string {
+	if x != nil {
+		return x.Address
 	}
 	return nil
 }
@@ -533,8 +808,35 @@ var file_requests_proto_rawDesc = []byte{
 	0x69, 0x74, 0x69, 0x61, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x14, 0x0a, 0x05,
 	0x74, 0x79, 0x70, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x05, 0x52, 0x05, 0x74, 0x79, 0x70,
 	0x65, 0x73, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x18, 0x02, 0x20, 0x01,
-	0x28, 0x0c, 0x52, 0x06, 0x73, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x28, 0x0c, 0x52, 0x06, 0x73, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x22, 0xa3, 0x01, 0x0a, 0x14, 0x56,
+	0x65, 0x72, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x4c, 0x0a, 0x0a, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65,
+	0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x2c, 0x2e, 0x68, 0x72, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x2e, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x2e, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72,
+	0x65, 0x50, 0x61, 0x69, 0x72, 0x52, 0x0a, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65,
+	0x73, 0x1a, 0x3d, 0x0a, 0x0d, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x50, 0x61,
+	0x69, 0x72, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02,
+	0x69, 0x64, 0x12, 0x1c, 0x0a, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65,
+	0x22, 0xdf, 0x01, 0x0a, 0x0e, 0x53, 0x75, 0x62, 0x6e, 0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x77, 0x6f, 0x72, 0x6b, 0x65, 0x72, 0x73, 0x18, 0x01,
+	0x20, 0x03, 0x28, 0x09, 0x52, 0x07, 0x77, 0x6f, 0x72, 0x6b, 0x65, 0x72, 0x73, 0x12, 0x1e, 0x0a,
+	0x0a, 0x62, 0x79, 0x73, 0x74, 0x61, 0x6e, 0x64, 0x65, 0x72, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28,
+	0x09, 0x52, 0x0a, 0x62, 0x79, 0x73, 0x74, 0x61, 0x6e, 0x64, 0x65, 0x72, 0x73, 0x12, 0x42, 0x0a,
+	0x09, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x65, 0x73, 0x18, 0x03, 0x20, 0x03, 0x28, 0x0b,
+	0x32, 0x24, 0x2e, 0x68, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x2e, 0x53, 0x75, 0x62, 0x6e,
+	0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x2e, 0x41, 0x64, 0x64, 0x72, 0x65,
+	0x73, 0x73, 0x50, 0x61, 0x69, 0x72, 0x52, 0x09, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x65,
+	0x73, 0x12, 0x16, 0x0a, 0x06, 0x72, 0x65, 0x77, 0x61, 0x72, 0x64, 0x18, 0x04, 0x20, 0x01, 0x28,
+	0x01, 0x52, 0x06, 0x72, 0x65, 0x77, 0x61, 0x72, 0x64, 0x1a, 0x37, 0x0a, 0x0b, 0x41, 0x64, 0x64,
+	0x72, 0x65, 0x73, 0x73, 0x50, 0x61, 0x69, 0x72, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72,
+	0x65, 0x73, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x09, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65,
+	0x73, 0x73, 0x22, 0x34, 0x0a, 0x12, 0x52, 0x65, 0x74, 0x75, 0x72, 0x6e, 0x43, 0x6f, 0x64, 0x65,
+	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x1e, 0x0a, 0x0a, 0x72, 0x65, 0x74, 0x75,
+	0x72, 0x6e, 0x43, 0x6f, 0x64, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x0a, 0x72, 0x65,
+	0x74, 0x75, 0x72, 0x6e, 0x43, 0x6f, 0x64, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -549,26 +851,33 @@ func file_requests_proto_rawDescGZIP() []byte {
 	return file_requests_proto_rawDescData
 }
 
-var file_requests_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
+var file_requests_proto_msgTypes = make([]protoimpl.MessageInfo, 13)
 var file_requests_proto_goTypes = []interface{}{
-	(*VerificationRequest)(nil),         // 0: hrequest.VerificationRequest
-	(*WorkerParameters)(nil),            // 1: hrequest.WorkerParameters
-	(*SubnetRequest)(nil),               // 2: hrequest.SubnetRequest
-	(*ConflictRequest)(nil),             // 3: hrequest.ConflictRequest
-	(*AvailabilitySetRequest)(nil),      // 4: hrequest.AvailabilitySetRequest
-	(*MultiaddressSetRequest)(nil),      // 5: hrequest.MultiaddressSetRequest
-	(*InitialRequest)(nil),              // 6: hrequest.InitialRequest
-	(*VerificationRequest_Keypair)(nil), // 7: hrequest.VerificationRequest.Keypair
+	(*VerificationRequest)(nil),                // 0: hrequest.VerificationRequest
+	(*WorkerParameters)(nil),                   // 1: hrequest.WorkerParameters
+	(*SubnetRequest)(nil),                      // 2: hrequest.SubnetRequest
+	(*ConflictRequest)(nil),                    // 3: hrequest.ConflictRequest
+	(*AvailabilitySetRequest)(nil),             // 4: hrequest.AvailabilitySetRequest
+	(*MultiaddressSetRequest)(nil),             // 5: hrequest.MultiaddressSetRequest
+	(*InitialRequest)(nil),                     // 6: hrequest.InitialRequest
+	(*VerificationResponse)(nil),               // 7: hrequest.VerificationResponse
+	(*SubnetResponse)(nil),                     // 8: hrequest.SubnetResponse
+	(*ReturnCodeResponse)(nil),                 // 9: hrequest.ReturnCodeResponse
+	(*VerificationRequest_Keypair)(nil),        // 10: hrequest.VerificationRequest.Keypair
+	(*VerificationResponse_SignaturePair)(nil), // 11: hrequest.VerificationResponse.SignaturePair
+	(*SubnetResponse_AddressPair)(nil),         // 12: hrequest.SubnetResponse.AddressPair
 }
 var file_requests_proto_depIdxs = []int32{
-	7, // 0: hrequest.VerificationRequest.keys:type_name -> hrequest.VerificationRequest.Keypair
-	1, // 1: hrequest.SubnetRequest.workerOpts:type_name -> hrequest.WorkerParameters
-	1, // 2: hrequest.AvailabilitySetRequest.availabilityOpts:type_name -> hrequest.WorkerParameters
-	3, // [3:3] is the sub-list for method output_type
-	3, // [3:3] is the sub-list for method input_type
-	3, // [3:3] is the sub-list for extension type_name
-	3, // [3:3] is the sub-list for extension extendee
-	0, // [0:3] is the sub-list for field type_name
+	10, // 0: hrequest.VerificationRequest.keys:type_name -> hrequest.VerificationRequest.Keypair
+	1,  // 1: hrequest.SubnetRequest.workerOpts:type_name -> hrequest.WorkerParameters
+	1,  // 2: hrequest.AvailabilitySetRequest.availabilityOpts:type_name -> hrequest.WorkerParameters
+	11, // 3: hrequest.VerificationResponse.signatures:type_name -> hrequest.VerificationResponse.SignaturePair
+	12, // 4: hrequest.SubnetResponse.addresses:type_name -> hrequest.SubnetResponse.AddressPair
+	5,  // [5:5] is the sub-list for method output_type
+	5,  // [5:5] is the sub-list for method input_type
+	5,  // [5:5] is the sub-list for extension type_name
+	5,  // [5:5] is the sub-list for extension extendee
+	0,  // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_requests_proto_init() }
@@ -662,7 +971,67 @@ func file_requests_proto_init() {
 			}
 		}
 		file_requests_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VerificationResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_requests_proto_msgTypes[8].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*SubnetResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_requests_proto_msgTypes[9].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ReturnCodeResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_requests_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*VerificationRequest_Keypair); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_requests_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VerificationResponse_SignaturePair); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_requests_proto_msgTypes[12].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*SubnetResponse_AddressPair); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -680,7 +1049,7 @@ func file_requests_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_requests_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   8,
+			NumMessages:   13,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
---
+++ response.go
@@ -0,0 +1,64 @@
+package hrequest
+
+import (
+	"github.com/gogo/protobuf/proto"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+/* NewVerificationResponse marshals a verification response value
+using a protocol buffer */
+func NewVerificationResponse(sigMap map[string][]byte) ([]byte, error) {
+	signatures := make([]*VerificationResponse_SignaturePair, 0, len(sigMap))
+	for id, signature := range sigMap {
+		newSignature := &VerificationResponse_SignaturePair{
+			Id:        id,
+			Signature: signature,
+		}
+		signatures = append(signatures, newSignature)
+	}
+
+	verfResponse := &VerificationResponse{Signatures: signatures}
+	response, err := proto.Marshal(verfResponse)
+	if err != nil {
+		return nil, &MarshalErr{simpleErr{err: err, msg: "MarshalErr in NewVerificationResponse"}}
+	}
+	return response, nil
+}
+
+/* NewSubnetResponse marshals a set of subnet response values using a
+protocol buffer */
+func NewSubnetResponse(workers []string, bystanders []string, addrs map[string][]ma.Multiaddr, reward float64) ([]byte, error) {
+	addresses := make([]*SubnetResponse_AddressPair, 0, len(addrs))
+	for id, addr := range addrs {
+		strAddr := make([]string, len(addr))
+		for i, maAddr := range addr {
+			strAddr[i] = maAddr.String()
+		}
+		newAddr := &SubnetResponse_AddressPair{
+			Id:      id,
+			Address: strAddr,
+		}
+		addresses = append(addresses, newAddr)
+	}
+
+	subResponse := &SubnetResponse{
+		Workers:    workers,
+		Bystanders: bystanders,
+		Addresses:  addresses,
+		Reward:     reward,
+	}
+	response, err := proto.Marshal(subResponse)
+	if err != nil {
+		return nil, &MarshalErr{simpleErr{err: err, msg: "MarshalErr in NewSubnetResponse"}}
+	}
+	return response, nil
+}
+
+func NewReturnCodeResponse(code int) ([]byte, error) {
+	returnResp := &ReturnCodeResponse{ReturnCode: int32(code)}
+	response, err := proto.Marshal(returnResp)
+	if err != nil {
+		return nil, &MarshalErr{simpleErr{err: err, msg: "MarshalErr in NewReturnCodeResponse"}}
+	}
+	return response, nil
+}
